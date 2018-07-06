4c390182b27cb0e693c2f29c2e4b165fe2d7ed82
Rys Vladislav <onesssse@gmail.com>
2018-07-07 02:12:24 +0300

    commitim normalno

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DestructorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<SomeObject> someObjects = new List<SomeObject>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                someObjects.Add(new SomeObject(i));
+                Console.WriteLine("Object created: ID -  " + someObjects[i]._id);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ SomeObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DestructorApp
+{
+    public class SomeObject
+    {
+        public int _id;
+
+        public SomeObject(int id)
+        {
+            _id = id;
+        }
+        
+        ~SomeObject()
+        {
+            Console.WriteLine("Object destroyed: ID - " + _id);
+            Console.ReadKey();
+        }
+
+    }
+}
